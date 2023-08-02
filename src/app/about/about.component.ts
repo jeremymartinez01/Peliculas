9c66968d4dcb648e4b23c52a5dd2fa70b2809da5
Jeremy Martinez <jjmartin@espol.edu.ec>
2023-08-02 14:16:39 -0500

    agregar pelis

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -27,8 +27,11 @@ if (myButton) {
 })
 export class AboutComponent {
   //Atributo con el tipo de dato de la interfaz
+  cantidad:number =8;
   db_brought=false;
   public data : Moviedata[] = [];
+  public data2 : Moviedata[] = [];
+
 	  
 
   //Inyección de dependencia del servicio
@@ -38,10 +41,26 @@ export class AboutComponent {
   ngOnInit() {
     this.dataProvider.getResponse().subscribe((response) => { 
       this.bringDB();
-      this.data = (response as Moviedata[]).slice(0,2); //uso slice para traer 2 datos, si quiero todos dejo sin slice
+      this.data2 = (response as Moviedata[]);
+      this.data = (response as Moviedata[]).slice(0,8); //uso slice para traer 8 pelis, si quiero todos dejo sin slice
     })
   }
+  mostrarMasPeliculas(): void {
+   
+      this.data =  this.data2.slice(this.cantidad,this.cantidad+8); //uso slice para traer 2 datos, si quiero todos dejo sin slice
+      this.cantidad=this.cantidad+8;
+    
+  }
+  mostrarMenosPeliculas(): void{
 
+    if(this.cantidad==8){
+      this.data=this.data2.slice(0,this.cantidad)
+    }else{
+      this.data=this.data2.slice(this.cantidad-16,this.cantidad-8);
+      this.cantidad=this.cantidad-8;
+    }
+    
+  }
   bringDB(){
     this.db_brought=true;
   }
